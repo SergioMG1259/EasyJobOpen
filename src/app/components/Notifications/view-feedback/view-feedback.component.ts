4f8adf94b0a5f1a6d0b13ec95f312da6e5e7c974
SergioMG1259 <89159015+SergioMG1259@users.noreply.github.com>
2021-10-22 01:07:44 -0500

    Update view-feedback.component.ts

--- src/app/components/Notifications/view-feedback/view-feedback.component.ts
+++ src/app/components/Notifications/view-feedback/view-feedback.component.ts
@@ -1,18 +1,1 @@
-import {Component, Inject, OnInit, Optional} from '@angular/core';
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 
-@Component({
-  selector: 'app-view-feedback',
-  templateUrl: './view-feedback.component.html',
-  styleUrls: ['./view-feedback.component.css']
-})
-export class ViewFeedbackComponent implements OnInit {
-  feedback:string
-  constructor(@Optional() @Inject(MAT_DIALOG_DATA)public data:string) {
-    this.feedback=data
-  }
-
-  ngOnInit(): void {
-  }
-
-}

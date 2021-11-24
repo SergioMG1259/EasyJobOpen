f3ce87557d33c13a719dfb10ab35bb96e7e3b118
Sebastian <49285362+symphony701@users.noreply.github.com>
2021-11-23 23:44:47 -0500

    Notification postulant complete

--- src/app/components/ApplicationProcess/visit-profile-postulant/visit-profile-postulant.component.ts
+++ src/app/components/ApplicationProcess/visit-profile-postulant/visit-profile-postulant.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Postulant} from "../../Postulant/model/postulant";
 import {PostulantServices} from "../../Services/Postulant-services";
+import {SpecialtyService} from "../../Services/SpecialtyExperience-service";
 import {Project} from "../../Project/model/project";
 import {ProjectsServices} from "../../Services/Project-services";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Router} from "@angular/router";
+import {InvitationComponent} from "../../Notifications/invitation/invitation.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-visit-profile-postulant',
@@ -14,6 +17,7 @@ import {Router} from "@angular/router";
 export class VisitProfilePostulantComponent implements OnInit {
   Postulant_data_visit:Postulant
   id:number
+  id_company:number
   name_postulant:string
   lastname_postulant:string
   description_postulant:string
@@ -21,10 +25,12 @@ export class VisitProfilePostulantComponent implements OnInit {
   specialty:string
   experience:string
   projects:any
+  specialtyexperience:any
   constructor(private route:ActivatedRoute,private router:Router,private postulantservices:PostulantServices,
-              private projectservices:ProjectsServices) {
+              private projectservices:ProjectsServices,private  specialties_experiences:SpecialtyService,public dialog:MatDialog){
     this.Postulant_data_visit={} as Postulant
     this.id=0
+    this.id_company=0
     this.name_postulant=""
     this.lastname_postulant=""
     this.description_postulant=""
@@ -32,29 +38,41 @@ export class VisitProfilePostulantComponent implements OnInit {
     this.specialty=""
     this.experience=""
     this.projects=[]
+    this.specialtyexperience=[]
   }
   get_postulant(){
     this.postulantservices.getById(this.id).subscribe(response=>{
       this.Postulant_data_visit=response
       this.name_postulant=this.Postulant_data_visit.name
-      this.lastname_postulant=this.Postulant_data_visit.lastname
+      this.lastname_postulant=this.Postulant_data_visit.lastName
       this.description_postulant=this.Postulant_data_visit.description
-      this.img_postulant=this.Postulant_data_visit.img_postulant
-      this.specialty=this.Postulant_data_visit.myspecialty
-      this.experience=this.Postulant_data_visit.myexperience
+      this.img_postulant=this.Postulant_data_visit.imgPostulant
+      /*this.specialty=this.Postulant_data_visit.myspecialty
+      this.experience=this.Postulant_data_visit.myexperience*/
+    })
+  }
+  get_specialties_experiences(){
+    this.specialties_experiences.get_by_idPostulant(this.id).subscribe(response=>{
+      this.specialtyexperience=response.content
     })
   }
   get_projects(){
     this.projectservices.getByOwner(this.id).subscribe(response=>{
-      this.projects=response
+      this.projects=response.content
+      console.log(response.content)
     })
   }
   gotoItems(id:number){
     this.router.navigate(['/MenuCompany/Visit-Project',id])
   }
+  Open_dialog_invitation(){
+    const dialogRef=this.dialog.open(InvitationComponent,{data:{id_postulant:this.id,id_company:this.id_company}})
+  }
   ngOnInit(): void {
     this.id=this.route.snapshot.params["id"]
+    this.id_company=this.route.snapshot.params["idcompany"]
     this.get_postulant()
+    this.get_specialties_experiences()
     this.get_projects()
   }
 
--- src/app/components/Evidence/edit-evidence/edit-evidence.component.ts
+++ src/app/components/Evidence/edit-evidence/edit-evidence.component.ts
@@ -15,16 +15,16 @@ export class EditEvidenceComponent implements OnInit {
   id_project_edit:number
   constructor(@Optional() @Inject(MAT_DIALOG_DATA)public data:Evidence,private evidenceservices:EvidenceServices) {
     this.id_evidence=data.id
-    this.title_edit=data.title_evidence
-    this.img_edit=data.img_evidence
-    this.description_edit=data.description_evidence
-    this.id_project_edit=data.id_project
+    this.title_edit=data.titleEvidence
+    this.img_edit=data.imgEvidence
+    this.description_edit=data.descriptionEvidence
+    this.id_project_edit=data.projectId
   }
   edit_evidence(){
     const data={
-      title_evidence:this.title_edit,
-      description_evidence:this.description_edit,
-      img_evidence:this.img_edit
+      titleEvidence:this.title_edit,
+      descriptionEvidence:this.description_edit,
+      imgEvidence:this.img_edit
     }
     this.evidenceservices.edit_evidence(this.id_evidence,data).subscribe(response=>{})
   }
